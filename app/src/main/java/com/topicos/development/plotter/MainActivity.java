c1ce9504ce2ce1e553f28b9027abde9a3688fed7
Miguel Angel <m.escobar_27@outlook.com>
2019-10-02 11:05:40 -0400

    redimensionando el lienzo

--- app/src/main/java/com/topicos/development/plotter/MainActivity.java
+++ app/src/main/java/com/topicos/development/plotter/MainActivity.java
@@ -3,12 +3,28 @@ package com.topicos.development.plotter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import com.topicos.development.plotter.utils.Lienzo;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Lienzo lienzo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lienzo = findViewById(R.id.surface_view);
+        redimensionar();
+    }
+
+    private void redimensionar() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        if (metrics.widthPixels < metrics.heightPixels)
+            lienzo.getLayoutParams().height = metrics.widthPixels;
+        else
+            lienzo.getLayoutParams().width = metrics.heightPixels;
     }
 }
--- app/src/main/java/com/topicos/development/plotter/utils/Lienzo.java
+++ app/src/main/java/com/topicos/development/plotter/utils/Lienzo.java
@@ -7,33 +7,41 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import com.topicos.development.plotter.model.Punto;
+
 public class Lienzo extends SurfaceView implements SurfaceHolder.Callback {
 
     private Lapiz lapiz;
+    private Punto punto;
+    private SurfaceHolder holder;
 
     public Lienzo(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.lapiz = new Lapiz();
+        this.getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        this.holder = holder;
+        this.punto = new Punto(getWidth() / 100, getHeight() / 100);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        this.holder = holder;
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
     }
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        punto.setX(event.getX());
+        punto.setY(event.getY());
         return super.onTouchEvent(event);
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-
-    }
 }
